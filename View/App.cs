362250b6f1836ce5919e6ecfa139120c7e445c65
Fatih Kartal <fatih.kartal@mobven.com>
2023-04-30 11:42:56 +0300

    Add project files.

---
+++ View/App.cs
@@ -0,0 +1,15 @@
+namespace Translator.View
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
